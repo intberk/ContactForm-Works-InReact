a88c36a0bfa4a3474a6ea96017ed84bfecad9871
Intberk <intberkmen@gmail.com>
2024-06-20 23:53:55 +0300

    Last Commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,22 +1,51 @@
 import React from 'react'
 import './Contact.css'
+import Swal from 'sweetalert2'
 
 const Contact = ()=> {
-    return(
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "2c806e13-e507-4863-92c8-57e2463545b8");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success"
+              });
+        }
+    };
+
+    return (
         <section className="contact">
-            <form>
+            <form onSubmit={onSubmit}>
                 <h2>Contact Form</h2>
                 <div className="input-box">
                     <label>Full Name</label>
-                    <input type="text" className="field" placeholder='Enter your name' required />
+                    <input type="text" className="field" placeholder='Enter your name' name='name' required />
                 </div>
                 <div className="input-box">
                     <label>Email Address</label>
-                    <input type="email" className="field" placeholder='Enter your email' required />
+                    <input type="email" className="field" placeholder='Enter your email' name='email' required />
                 </div>
                 <div className="input-box">
                     <label>Your Message</label>
-                    <textarea name="" id="" className="field mess" placeholder='Enter your message' required></textarea>
+                    <textarea name="message" className="field mess" placeholder='Enter your message' required></textarea>
                 </div>
                 <button type='submit'>Send Message</button>
             </form>
